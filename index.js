1d3fb882b4f0a1ee4c11565f658d47e0087026aa
mardIn smidty <147711727+Martin336699@users.noreply.github.com>
2025-01-25 12:37:16 +0100

    finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer()
 
 var app = express();
 
@@ -11,7 +13,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  if (!file) {
+    res.json("No file uploaded");
+    return;
+  }
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
